9caf902318434a0eaf2c81da1c52bf6956752772
eeazzy <khs2498@gmail.com>
2024-12-03 19:59:46 +0900

    feat: 색상표 추가

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+    red: "#FF0000",
+    blue: "#587DEB",
+    gray_blue: "#6B7AA6",
+    gray_blue_30: "rgba(107, 122, 166, 0.3)",
+    navy: "#1E3D69",
+    light_blue_10: "rgba(191, 201, 228, 0.1)",
+    dark_gray: "#979797",
+    gray: "#D9D9D9",
+};
+
+export default colors;
+  
